50a3da0af6253d424266e421d5e6c4c036e7c5e9
mimidios <masterjimm8@gmail.com>
2023-05-10 23:35:54 +0300

    made it auto open vlc

--- muffyroll.sh
+++ muffyroll.sh
@@ -2,5 +2,15 @@
 
 query=$(printf '%s' "$*" | tr ' ' '+' )
 resolution='+1080p'
+
 magnet=$(echo $(curl -s "https://nyaa.si/?f=0&c=1_2&q=$query$resolution&s=seeders&o=desc") | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" | head -n 1)
-peerflix "$magnet"
+
+if [ "$magnet" == "" ]; then
+  echo "torrent not found for $query"
+  exit 1
+else
+  echo "found torrent for $query" 
+  echo "opening vlc.."
+  peerflix "$magnet" --vlc >> /dev/null
+fi
+
